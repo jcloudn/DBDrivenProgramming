9f021d209e9ef2aa041cd8195895f9dd9afc5517
jcloudn <jeanne.cloudnadeau@smail.rasmussen.edu>
2018-01-19 19:43:09 -0600

    added mod3 lab
    Created Module 3 folder and added module 3 lab assignment

---
+++ Module3Upload/Module3_Lab.php
@@ -0,0 +1,34 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<title>Data Types</title>
+</head>
+<body>
+
+
+
+
+<h1>Showing Var_Dump Examples</h1>
+
+
+
+<?php
+//array example
+$colors = array("Red","White","Blue");
+var_dump($colors);
+//float example
+$samplefloat = 2017.465;
+var_dump($samplefloat);
+//integer example
+$sampleinteger = 2017;
+var_dump($sampleinteger);
+//string example
+$samplestring = "Last year was 2017.";
+var_dump($samplestring);
+?>
+
+<div data-role="footer" data-theme="b">
+<h4>MARVEL ADVENTURES &copy; 2018</h4>
+</div>
+</body>
+</html>
