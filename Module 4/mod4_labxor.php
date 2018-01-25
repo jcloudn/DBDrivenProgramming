c3e0ac1ce051d1ae6564a880f9822587b2498d42
jcloudn <35573678+jcloudn@users.noreply.github.com>
2018-01-24 20:04:54 -0600

    Added mod4_lab xor operator
    Added mod4_lab xor operator

---
+++ Module 4/mod4_labxor.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+//xor operator example
+$variable1 = 100;
+$variable2 = 50;
+
+if ($variable1 == 100 xor $variable2 == 80) {
+    echo "January is a cold month!";
+}
+?>
+
+</body>
+</html>
