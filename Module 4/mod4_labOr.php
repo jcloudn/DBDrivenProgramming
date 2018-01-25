d38c2603541ad33f68529425302fd7fb405d700c
jcloudn <35573678+jcloudn@users.noreply.github.com>
2018-01-24 20:03:12 -0600

    Added mod4_lab Or operator
    Added mod4_lab Or operator

---
+++ Module 4/mod4_labOr.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<body>
+  
+<?php
+//or operator example
+$variable1 = 100;
+$variable2 = 50;
+
+if ($variable1 == 100 or $variable2 == 50) {
+    echo "I survived the blizzard of 2018!";
+}
+?>
+
+</body>
+</html>
