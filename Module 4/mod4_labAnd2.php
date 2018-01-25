f7bd9467229e423aac536a887415c3b58be9909d
jcloudn <35573678+jcloudn@users.noreply.github.com>
2018-01-24 20:01:27 -0600

    Added mod4_lab And2 operator
    Added mod4_lab And2 operator

---
+++ Module 4/mod4_labAnd2.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+
+//&& operator example
+$variable1 = 100;
+$variable2 = 50;
+
+if ($variable1 == 100 && $variable2 == 50) {
+    echo "Hello world!";
+}
+?>
+
+</body>
+</html>
