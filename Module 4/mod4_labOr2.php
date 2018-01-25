a5e65fbe940b21804d3a2b9061110e04dd493e92
jcloudn <35573678+jcloudn@users.noreply.github.com>
2018-01-24 20:04:03 -0600

    Added mod4_lab Or2 operator
    Added mod4_lab Or2 operator

---
+++ Module 4/mod4_labOr2.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+
+//|| operator example
+$admin = true;
+$moderator = false;
+
+if ($admin == true || $moderator == true) {
+    echo "True";
+} else {
+  echo "False";
+}
+
+?>
+
+</body>
+</html>
