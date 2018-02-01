61e03d40b0e82381af67029a30d9eafde84e17af
jcloudn <35573678+jcloudn@users.noreply.github.com>
2018-01-31 21:28:02 -0600

    created mod 5 course project

---
+++ Module 5/mod5_Courseproject.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<head>
+<title>Break in the Code</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="styles/custom.css" />
+<link rel="stylesheet" href="themes/rasmussenthemeroller.min.css" />
+<link rel="stylesheet" href="themes/jquery.mobile.icons.min.css" />
+<link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.5/jquery.mobile.structure-1.4.5.min.css" />
+<script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
+<script src="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
+</head>
+<body>
+  <div id="page" data-role="page" data-theme="b" >
+<div data-role="header" data-theme="b">
+<h1>I need a break!</h1>
+</div>
+  <?php
+
+  for ($i = 100 ; $i >= 01; $i-=5){
+    if ($i==50){
+      break;
+    }
+
+    echo "The number is: $i", '</br>';
+}
+
+
+?>
+
+<div data-role="footer" data-theme="b">
+	 <h4>MARVEL ADVENTURES &copy; 2018</h4>
+</div>
+</body>
+</html>
